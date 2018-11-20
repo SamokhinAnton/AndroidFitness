c8d6bc1a3276d866ff30be861c5da8b7582b3799
Anton Samokhin <anton.samokhin@globallogic.com>
2018-11-20 15:48:02 +0200

    category controller actions

--- Fitness.WebApi/Controllers/CategoryController.cs
+++ Fitness.WebApi/Controllers/CategoryController.cs
@@ -20,12 +20,42 @@ namespace Fitness.WebApi.Controllers
         }
 
 
-        [HttpGet]
+        [HttpGet("")]
         public async Task<IActionResult> Get()
         {
             var categories = await _category.GetAllAsync();
             return Ok(categories);
         }
 
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _category.GetByIdAsync(id);
+            return Ok(category);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CategoryModel category)
+        {
+            await _category.Create(category);
+            return Created("Category/Create", category);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(CategoryModel category)
+        {
+            await _category.Update(category);
+            var categoryResult = await _category.GetByIdAsync(category.Id);
+            return Ok(categoryResult);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _category.Delete(id);
+            var categories = _category.GetAllAsync();
+            return Ok(categories);
+        }
     }
 }
